e47ca9564a842e96f64fda22329abf12fa92c389
Park kyungsun <asinta@naver.com>
2014-07-10 01:06:43 +0900

    노래선택하면 재생(정지기능 없음) close #6

--- app/src/main/java/me/nworks/nl/tento/fragments/PlaylistFragment.java
+++ app/src/main/java/me/nworks/nl/tento/fragments/PlaylistFragment.java
@@ -1,8 +1,11 @@
 package me.nworks.nl.tento.fragments;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.app.FragmentTransaction;
 import android.content.ContentResolver;
 import android.database.Cursor;
+import android.media.MediaPlayer;
 import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
@@ -10,15 +13,28 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import me.nworks.nl.tento.MainActivity;
 import me.nworks.nl.tento.R;
 
+import static android.widget.AdapterView.OnItemClickListener;
 
-public class PlaylistFragment extends Fragment {
 
+public class PlaylistFragment extends Fragment{
+
+    private ArrayList<String> list = new ArrayList<String>();
+    private ArrayList<String> path = new ArrayList<String>();
+    private MediaPlayer mp = new MediaPlayer();
     @Override
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -28,6 +44,8 @@ public class PlaylistFragment extends Fragment {
         Bundle args = getArguments();
         Activity activity = getActivity();
         ContentResolver resolver = activity.getContentResolver();
+        ArrayAdapter<String> listadapter;
+
         String selection = MediaStore.Audio.Media.IS_MUSIC + " != 0";
         String[] projection = {
                 MediaStore.Audio.Media._ID,
@@ -44,27 +62,36 @@ public class PlaylistFragment extends Fragment {
                 selection,
                 null,
                 null);
-        TableLayout playlistTable = (TableLayout) rootView.findViewById(R.id.playlist_table);
-        while(cursor.moveToNext()) {
-            TableRow row = new TableRow(activity);
-            TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
-            row.setLayoutParams(lp);
-            TextView title = new TextView(activity);
-            TextView artist = new TextView(activity);
-            title.setText(cursor.getString(1));
-            artist.setText(cursor.getString(2));
 
-            row.addView(title);
-            row.addView(artist);
-            playlistTable.addView(row);
-            Log.d("hello", cursor.getString(0) + "||"
-                    + cursor.getString(1) + "||"
-                    + cursor.getString(2) + "||"
-                    + cursor.getString(3) + "||"
-                    + cursor.getString(4) + "||"
-                    + cursor.getString(5));
+        ListView listview = (ListView)rootView.findViewById(R.id.playlist_listview);
+        listadapter = new ArrayAdapter<String>(activity, android.R.layout.simple_list_item_1, list);
+        listview.setAdapter(listadapter);
+        listview.setOnItemClickListener(new ListViewItemClickListener());
+
+           while(cursor.moveToNext()) {
+               list.add(cursor.getString(1)+" - "+ cursor.getString(2));
+               path.add(cursor.getString(3));
         }
         return rootView;
     }
+    private class ListViewItemClickListener implements AdapterView.OnItemClickListener
+    {
+        @Override
+        public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,long arg3) {
+            // TODO Auto-generated method stub
+            Log.d("aaa",String.valueOf(arg2)+"번째 : " + path.get(arg2));
+            playSong(path.get(arg2));
+        }
+    }
 
+    private void playSong(String path){
+        try{
+            mp.reset();
+            mp.setDataSource(path);
+            mp.prepare();
+            mp.start();
+        }catch(Exception e){
+            Log.d("aaa",e.toString());
+        }
+    }
 }
