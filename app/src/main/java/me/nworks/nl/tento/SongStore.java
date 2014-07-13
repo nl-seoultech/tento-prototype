ec2b73508573a032ecdc3d24db71f800942ce123
Kang HyoJun <admire9@gmail.com>
2014-07-13 17:52:38 +0900

    MediaStore에서 가져온 내용을 담는 SongStore 구현
    close #12

---
+++ app/src/main/java/me/nworks/nl/tento/SongStore.java
@@ -0,0 +1,143 @@
+package me.nworks.nl.tento;
+
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.provider.MediaStore;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class SongStore {
+
+    /**
+     * MediaStore 에서 검색해온 mp3의 정보를 담는 클래스
+     */
+    public class Song {
+
+        // 안드로이드 MediaStore 에서 mp3의 고유 id
+        private String id;
+
+        // mp3의 아티스트
+        private String artist;
+
+        // mp3의 제목
+        private String title;
+
+        // mp3의 경로
+        private String path;
+
+        // mp3 의 display 이름
+        private String displayName;
+
+        // mp3의 길이
+        private String duration;
+
+        public Song(Cursor cursor) {
+            id = cursor.getString(0);
+            artist = cursor.getString(1);
+            title = cursor.getString(2);
+            path = cursor.getString(3);
+            displayName = cursor.getString(4);
+            duration = cursor.getString(5);
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public String getArtist() {
+            return artist;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public String getPath() {
+            return path;
+        }
+
+        public String getDisplayName() {
+            return displayName;
+        }
+
+        public String getDuration() {
+            return duration;
+        }
+    }
+
+    private static ArrayList<Song> songs;
+
+    private static HashMap<String, Integer> songIndexById;
+
+    private static HashMap<String, Integer> songIndexByPath;
+
+    private Activity activity;
+
+    public SongStore(Activity activity) {
+        this.activity = activity;
+        initializeSongs(false);
+    }
+
+    /**
+     * songs 에 사용자가 가지고있는 노래 목록을 저장합니다.
+     *
+     * @param refresh 만약에 true이면 songs 에 노래 목록이 저장되있어도 초기화후 새로 저장합니다.
+     */
+    public void initializeSongs(boolean refresh) {
+        if(refresh || songs.isEmpty()) {
+            songs = new ArrayList<Song>();
+            songIndexById = new HashMap<String, Integer>();
+            songIndexByPath = new HashMap<String, Integer>();
+            String selection = MediaStore.Audio.Media.IS_MUSIC + " != 0";
+            String[] projection = {
+                    MediaStore.Audio.Media._ID,
+                    MediaStore.Audio.Media.ARTIST,
+                    MediaStore.Audio.Media.TITLE,
+                    MediaStore.Audio.Media.DATA,
+                    MediaStore.Audio.Media.DISPLAY_NAME,
+                    MediaStore.Audio.Media.DURATION
+            };
+            ContentResolver resolver = this.activity.getContentResolver();
+            Cursor cursor = resolver.query(
+                    MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                    projection,
+                    selection,
+                    null,
+                    null);
+
+            while(cursor.moveToNext()) {
+                songs.add(new Song(cursor));
+            }
+
+            // 나중에 findSongByPath나 findSongById 같은걸 사용하기위해서 id와 path를 HashMap에 저장해놓습니다.
+            for(int i = 0; i < songs.size(); i++) {
+                songIndexById.put(songs.get(i).getId(), i);
+                songIndexByPath.put(songs.get(i).getPath(), i);
+            }
+        }
+    }
+
+    public Song findSongByPath(String path) {
+        return songs.get(songIndexByPath.get(path));
+    }
+
+    public Song findSongById(String id) {
+        return songs.get(songIndexById.get(id));
+    }
+
+    public Song findSongByIndex(int i) {
+        return songs.get(i);
+    }
+
+    public ArrayList<String> getSongNames() {
+        ArrayList<String> names = new ArrayList<String>();
+
+        for(Song s : songs) {
+            names.add(String.format("%s - %s", s.getTitle(), s.getArtist()));
+        }
+
+        return names;
+    }
+}
--- app/src/main/java/me/nworks/nl/tento/fragments/PlaylistFragment.java
+++ app/src/main/java/me/nworks/nl/tento/fragments/PlaylistFragment.java
@@ -2,8 +2,7 @@ package me.nworks.nl.tento.fragments;
 
 import android.app.Activity;
 import android.content.ContentResolver;
-import android.database.Cursor;
-import android.provider.MediaStore;
+import android.media.MediaPlayer;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -11,66 +10,58 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
-import java.util.ArrayList;
 
 import me.nworks.nl.tento.PlaySongService;
 import me.nworks.nl.tento.R;
+import me.nworks.nl.tento.SongStore;
 
 
 public class PlaylistFragment extends Fragment{
 
-    private ArrayList<String> list = new ArrayList<String>(); //음악 파일 목록을 담을 배열
-    private ArrayList<String> path = new ArrayList<String>(); //음악 파일의 경로를 담을 배열
-
     private PlaylistInterface pi;
+
+    private MediaPlayer mp = new MediaPlayer();
+
+    private SongStore songStore;
+
     @Override
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-
 
         View rootView = inflater.inflate(R.layout.fragment_playlist, container, false);
         Bundle args = getArguments();
         Activity activity = getActivity();
         ContentResolver resolver = activity.getContentResolver();
         ArrayAdapter<String> listadapter; //Listview인 list와 list배열을 연결할 어댑터
-
-        String selection = MediaStore.Audio.Media.IS_MUSIC + " != 0";
-        String[] projection = {
-                MediaStore.Audio.Media._ID,
-                MediaStore.Audio.Media.ARTIST,
-                MediaStore.Audio.Media.TITLE,
-                MediaStore.Audio.Media.DATA,
-                MediaStore.Audio.Media.DISPLAY_NAME,
-                MediaStore.Audio.Media.DURATION
-        };
-
-        Cursor cursor = resolver.query(
-                MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
-                projection,
-                selection,
-                null,
-                null);
-
-        ListView listview = (ListView)rootView.findViewById(R.id.listPlaylist);
-        listadapter = new ArrayAdapter<String>(activity, android.R.layout.simple_list_item_1, list);
+        songStore = new SongStore(activity);
+        ListView listview = (ListView)rootView.findViewById(R.id.playlist_listview);
+        listadapter = new ArrayAdapter<String>(
+                activity,
+                android.R.layout.simple_list_item_1,
+                songStore.getSongNames());
         listview.setAdapter(listadapter);
         listview.setOnItemClickListener(new ListViewItemClickListener()); //리스트 클릭 이벤트
 
-           while(cursor.moveToNext()) {
-               list.add(cursor.getString(1)+" - "+ cursor.getString(2));
-               path.add(cursor.getString(3));
-        }
+        Button btnPause = (Button) rootView.findViewById(R.id.btnPause);
+        btnPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mp.pause();
+            }
+        });
+
         return rootView;
     }
     private class ListViewItemClickListener implements AdapterView.OnItemClickListener
     {
         @Override
-        public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,long arg3) {
+        public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
             // TODO Auto-generated method stub
-            PlaySongService.file = path.get(arg2);
-            pi.startSong(path.get(arg2));
-
+            String path = songStore.findSongByIndex(arg2).getPath();
+            PlaySongService.file = path;
+            pi.startSong(path);
         }
     }
 
